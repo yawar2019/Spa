eafc38efb25f3958147632bc3d136cce267c0451
Sujanitha S <sujanithagalaxy@gmail.com>
2021-03-24 22:51:17 -0500

    Commit test by Suja

--- Spa/Controllers/HomeController.cs
+++ Spa/Controllers/HomeController.cs
@@ -6,6 +6,9 @@ using System.Web.Mvc;
 
 namespace Spa.Controllers
 {
+    /// <summary>
+    /// Test by Suja 3/24
+    /// </summary>
     public class HomeController : Controller
     {
         // GET: Home is safe
